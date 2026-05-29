36f7878fc69cd7e465f50c8a0a90d1fd5d84c828
nano-ui <kou0168na1121@outlook.jp>
2026-05-29 14:14:19 +0900

    2026/05/29 SceneManagerクラス実装

--- Source/Scene/SceneManager.h
+++ Source/Scene/SceneManager.h
@@ -1,5 +1,40 @@
 #pragma once
+
+#include <memory>
+
+class Scene;
+
 class SceneManager
 {
+public:
+	//シングルトンを取得
+	static SceneManager& Instance();
+
+	//シーン遷移
+	void ChangeScene(std::unique_ptr<Scene> next_scene);
+
+	//更新処理
+	void Update(float elapsed_time);
+
+	//描画処理
+	void Render(float elapsed_time);
+
+	//マネージャーの終了処理
+	void Finalize();
+
+private:
+	//コンストラクタ
+	SceneManager();
+
+	//デストラクタ
+	~SceneManager();
+
+	//コピーと代入を禁止
+	SceneManager(const SceneManager&) = delete;
+	SceneManager& operator = (const SceneManager&) = delete;
+
+private:
+	std::unique_ptr<Scene> current_scene;	//実行中のシーン
+	std::unique_ptr<Scene> reserved_scene;	//予約シーン
 };
 
