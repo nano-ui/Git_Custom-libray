93510d66299c786ffa841d1098e3b103264185a0
nano-ui <kou0168na1121@outlook.jp>
2026-05-29 10:18:26 +0900

    2026/05/29 ObjectManagerクラス実装

---
+++ Source/GameObjects/ObjectManager.cpp
@@ -0,0 +1,79 @@
+#include "ObjectManager.h"
+#include "GameObject.h"
+
+//コンストラクタ
+ObjectManager::ObjectManager()
+{
+}
+
+//デストラクタ
+ObjectManager::~ObjectManager()
+{
+	Clear();
+}
+
+//オブジェクトの登録
+void ObjectManager::Register(std::unique_ptr<GameObject> object)
+{
+	//オブジェクトの初期化とリストに追加
+	object->Initialize();
+	game_objects.push_back(std::move(object));
+}
+
+//全オブジェクトの更新処理
+void ObjectManager::Update(float elapsed_time)
+{
+	//生存している全オブジェクトの更新処理
+	for (auto& object : game_objects)
+	{
+		if (object->IsActive())
+		{
+			object->Update(elapsed_time);
+		}
+	}
+
+	//不要なオブジェクトの削除
+	RemoveInactiveObject();
+}
+
+//全オブジェクトの描画処理
+void ObjectManager::Render(ID3D11DeviceContext* context)
+{
+	//生存している全オブジェクトの描画処理
+	for (auto& object : game_objects)
+	{
+		if (object->IsActive())
+		{
+			object->Render(context);
+		}
+	}
+}
+
+//全オブジェクトのImGuiデバッグ描画処理
+void ObjectManager::RenderGui()
+{
+	//生存している全オブジェクトのImGui描画処理
+	for (auto& object : game_objects)
+	{
+		if (object->IsActive())
+		{
+			object->RenderGui();
+		}
+	}
+}
+
+//無効になったオブジェクトの削除処理
+void ObjectManager::RemoveInactiveObject()
+{
+	for (auto iterator = game_objects.begin(); iterator != game_objects.end();)
+	{
+		if ((*iterator)->IsActive())
+		{
+			iterator = game_objects.erase(iterator);
+		}
+		else
+		{
+			iterator++;
+		}
+	}
+}
--- Source/GameObjects/ObjectManager.h
+++ Source/GameObjects/ObjectManager.h
@@ -9,8 +9,32 @@ struct ID3D11DeviceContext;
 class ObjectManager
 {
 public:
+	//コンストラクタ
+	ObjectManager();
+
+	//デストラクタ
+	~ObjectManager();
+
+	//オブジェクトの登録
+	void Register(std::unique_ptr<GameObject> object);
+
+	//全オブジェクトの更新処理
+	void Update(float elapsed_time);
+
+	//全オブジェクトの描画処理
+	void Render(ID3D11DeviceContext* context);
+
+	//全オブジェクトのImGuiデバッグ描画処理
+	void RenderGui();
+
+	//全オブジェクトの強制削除処理
+	void Clear() { game_objects.clear(); }
 
 private:
+	//無効になったオブジェクトの削除処理
+	void RemoveInactiveObject();
 
+private:
+	std::vector<std::unique_ptr<GameObject>> game_objects;	//ゲームオブジェクトを管理する配列
 };
 
