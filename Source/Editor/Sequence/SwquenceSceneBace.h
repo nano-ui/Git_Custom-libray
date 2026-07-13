96b9e3ab12e41cadde2841a09036a8a91d7588bc
nano-ui <kou0168na1121@outlook.jp>
2026-07-13 12:27:24 +0900

    2026/07/13 SequenceSceneBaceクラス実装

---
+++ Source/Editor/Sequence/SwquenceSceneBace.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class SwquenceSceneBace
+{
+public:
+	//仮想デストラクタ
+	virtual ~SwquenceSceneBace() = default;
+
+	//初期化
+	virtual void Initialize() = 0;
+
+	//更新
+	virtual void Update(float elapsed_time) = 0;
+
+	//ImGui描画
+	virtual void RenderGui() = 0;
+};
+
