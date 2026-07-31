ee58fd5b8943f839d33d6d7fe0ceea02f87c051f
nano-ui <kou0168na1121@outlook.jp>
2026-07-31 11:38:12 +0900

    2026/07/31 JsonSerializerクラス編集
    ・不要になった関数や処理を削除

--- Source/Serialization/JsonSerializer.h
+++ Source/Serialization/JsonSerializer.h
@@ -48,9 +48,6 @@ public:
 	
 	//JSONオブジェクトから値を復元
 	virtual void LoadFrom(const nlohmann::json& json_data, const std::string& property_name) = 0;
-
-	//ImGui描画
-	virtual void DrawImGui(const std::string& property_name) = 0;
 };
 
 template <typename T>
@@ -81,52 +78,6 @@ public:
 		}
 	}
 
-	//ImGui描画
-	void DrawImGui(const std::string& property_name)override
-	{
-		//型に応じた関数をコンパイル時に結合
-		if constexpr(std::is_same_v<T, int>)
-		{
-			constexpr float drag_speed = 1.0f;
-			ImGui::DragInt(property_name.c_str(),data_pointer,drag_speed);
-		}
-		else if constexpr (std::is_same_v<T, float>)
-		{
-			constexpr float drag_speed = 0.1f;
-			ImGui::DragFloat(property_name.c_str(), data_pointer, drag_speed);
-		}
-		else if constexpr (std::is_same_v<T, DirectX::XMFLOAT3>)
-		{
-			constexpr float drag_speed = 0.1f;
-			ImGui::DragFloat3(property_name.c_str(), &data_pointer->x, drag_speed);
-		}
-		else if constexpr (std::is_same_v<T, DirectX::XMFLOAT4>)
-		{
-			constexpr float drag_speed = 0.1f;
-			ImGui::DragFloat4(property_name.c_str(), &data_pointer->x, drag_speed);
-		}
-		else if constexpr (std::is_same_v<T, bool>)
-		{
-			ImGui::Checkbox(property_name.c_str(), data_pointer);
-		}
-		else if constexpr (std::is_same_v<T, std::string>)
-		{
-			constexpr size_t string_buffer_size = 256;
-			char edit_buffer[string_buffer_size];
-
-			strncpy_s(edit_buffer, string_buffer_size, data_pointer->c_str(), _TRUNCATE);
-
-			if (ImGui::InputText(property_name.c_str(), edit_buffer, string_buffer_size))
-			{
-				*data_pointer = edit_buffer;
-			}
-		}
-		else
-		{
-			ImGui::Text("Unsupported Type: %s", property_name.c_str());
-		}
-	}
-
 private:
 	T* data_pointer;	//対象のポインタ
 };
@@ -140,17 +91,6 @@ public:
 	//デストラクタ
 	~JsonSerializer();
 
-	//セーブ・ロードの自動処理対象にしたい名とポインタをシステムに登録
-	template<typename T>
-	void RegisterVariable(const std::string& property_name, T* target_variable, const std::string& category_name = "Default")
-	{
-		PropertyData new_data;
-		new_data.name = property_name;
-		new_data.category = category_name;
-		new_data.property_interface = std::make_unique<TypedProperty<T>>(target_variable);
-		registered_properties.push_back(std::move(new_data));
-	}
-
 	//JSON形式でデータをファイルへ書き出す
 	void SaveToFile(const std::string& file_path);
 
@@ -163,15 +103,11 @@ public:
 	//JSONオブジェクトからデータを直接読み込む
 	void LoadFromObject(const nlohmann::json& root_json);
 
-	//登録された全のUIを一括描画
-	void RenderGui();
-
 private:
 	//個別のプロパティ情報をまとめる内部構造体
 	struct PropertyData
 	{
 		std::string name;		//名
-		std::string category;	//所属するグループ
 		std::unique_ptr<IProperty> property_interface;	//操作ポインタ
 	};
 
