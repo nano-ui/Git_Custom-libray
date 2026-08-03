111bc5615275f377c848e0b5970570a2831c0e4f
nano-ui <kou0168na1121@outlook.jp>
2026-08-03 09:59:41 +0900

    AnimationSequencerComponentクラス編集
    ・ループ再生ができるように修正

--- Source/Gameplay/Components/Animation/AnimationSequencerComponent.cpp
+++ Source/Gameplay/Components/Animation/AnimationSequencerComponent.cpp
@@ -62,6 +62,14 @@ void AnimationSequencerComponent::Update(float elapsed_time)
 	//経過時間を加算
 	current_sequence_time += elapsed_time;
 
+	float anim_duration = shared_model->GetAnimationDuration();
+
+	if (anim_duration > 0.0f)
+	{
+		if (current_sequence_time >= anim_duration)current_sequence_time = std::fmod(current_sequence_time, anim_duration);
+		else OutputDebugStringA("[AnimationSequencerComponent 警告] Update: アニメーションの総再生時間が0以下です。\n");
+	}
+
 	//現在のアニメーションに対応する速度カーブからモデル再生時間を算出
 	float integrated_time_b = GetIntegratedModelTime(current_sequence_time);
 
